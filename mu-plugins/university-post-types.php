6490bae230e96301a2bf1226763b820917f1f5de
harshgajipara <60835353+harshgajipara@users.noreply.github.com>
2020-08-20 11:42:29 +0530

    Create university-post-types.php

---
+++ mu-plugins/university-post-types.php
@@ -0,0 +1,65 @@
+<?php 
+
+function university_post_types(){
+
+	register_post_type('campus',array(
+		'supports'=>array('title','editor','excerpt'),
+		'rewrite'=>array('slug'=>'campuses'),
+		'has_archive'=>true,
+		'public'=>true,
+		'labels'=>array(
+			'name'=>'Campuses',
+			'add_new_item'=>'Add New Campus',
+			'edit_item'=>'Edit Campuses',
+			'all_items'=>'All Campuses',
+			'singular_name'=>'Campus'
+		),	
+		'menu_icon'=>'dashicons-location-alt'
+	));
+
+	register_post_type('event',array(
+		'supports'=>array('title','editor','excerpt'),
+		'rewrite'=>array('slug'=>'events'),
+		'has_archive'=>true,
+		'public'=>true,
+		'labels'=>array(
+			'name'=>'Events',
+			'add_new_item'=>'Add New Event',
+			'edit_item'=>'Edit Events',
+			'all_items'=>'All Events',
+			'singular_name'=>'Event'
+		),
+		'menu_icon'=>'dashicons-calendar'
+	));
+
+
+	register_post_type('program',array(
+		'supports'=>array('title','editor'),
+		'rewrite'=>array('slug'=>'programs'),
+		'has_archive'=>true,
+		'public'=>true,
+		'labels'=>array(
+			'name'=>'Programs',
+			'add_new_item'=>'Add New Program',
+			'edit_item'=>'Edit Program',
+			'all_items'=>'All Programs',
+			'singular_name'=>'Program'
+		),
+		'menu_icon'=>'dashicons-awards'
+	));
+
+	register_post_type('professor',array(
+		'supports'=>array('title','editor','thumbnail'),
+		'public'=>true,
+		'labels'=>array(
+			'name'=>'professor',
+			'add_new_item'=>'Add New Professor',
+			'edit_item'=>'Edit Professor',
+			'all_items'=>'All Professor',
+			'singular_name'=>'Professor'
+		),	
+		'menu_icon'=>'dashicons-welcome-learn-more'
+	));
+}
+
+add_action('init','university_post_types');
